2e30533d77d0a6fa0a11fb895690429420b0d91c
bhargav-babloo <bhargavsairam1996@gmail.com>
2018-11-03 12:36:18 +0530

    assignment

---
+++ asmt4.py
@@ -0,0 +1,14 @@
+
+def parrot(a,b):
+    if a== True and b<7:
+        print(True)
+    elif a== True and b>20:
+        print(True)
+    else:
+        print(False)
+parrot(a= bool(input("Enter True Or False: ")),b= int(input("Enter Time 0-23: ")))
+
+
+
+
+
